dccdd32d1a840283be22c2fe7d0b6164d0321fb9
Gustavo Laismann <gu.laismann2013@gmail.com>
2026-03-07 02:46:16 -0300

    completed PUT and DELETE routes for CRUD API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -33,6 +34,40 @@ app.post('/tickets', (req, res) => {
     tickets.push(newTicket)
 
     res.json(newTicket)
+})
+
+
+app.put('/tickets/:id', (req, res) => {
+
+    const ticketId = parseInt(req.params.id);
+    const foundTicket = tickets.find(ticket => ticket.id === ticketId)
+
+    if (foundTicket) {
+        foundTicket.title = req.body.title;
+        foundTicket.status = req.body.status;
+
+
+        res.json(foundTicket)
+    } else {
+        res.status(404).json({ error: "Ticket not found." })
+    }
+
+
+})
+
+app.delete('/tickets/:id', (req, res) => {
+    const ticketId = parseInt(req.params.id);
+    const foundIndexTicket = tickets.findIndex(ticket => ticket.id === ticketId)
+
+    if (foundIndexTicket >= 0) {
+
+        tickets.splice(foundIndexTicket, 1)
+        res.send("Removed Ticket ID: " + ticketId)
+
+    } else {
+        res.status(404).json({ error: "Ticket not found." })
+    }
+
 })
 
 app.listen(port, () => {
